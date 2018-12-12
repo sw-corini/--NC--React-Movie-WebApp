dfff4c4f6637c38fd86ed5c52de5449b16a7090a
sw <genie_s2@naver.com>
2018-12-13 08:45:31 +0900

    Updates

---
+++ precache-manifest.3dd4ccf50654ae635de37393d77807d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23bffc5ae723f64d0652",
+    "url": "/sw-corini/NC--React-Movie-WebApp/static/css/main.adf0b33d.chunk.css"
+  },
+  {
+    "revision": "23bffc5ae723f64d0652",
+    "url": "/sw-corini/NC--React-Movie-WebApp/static/js/main.23bffc5a.chunk.js"
+  },
+  {
+    "revision": "c0a994b21ba8d5f22cba",
+    "url": "/sw-corini/NC--React-Movie-WebApp/static/js/1.c0a994b2.chunk.js"
+  },
+  {
+    "revision": "96ed3476a665fbbf37e6",
+    "url": "/sw-corini/NC--React-Movie-WebApp/static/js/runtime~main.96ed3476.js"
+  },
+  {
+    "revision": "60cd4f5b6217b7edb160493402fd2c1a",
+    "url": "/sw-corini/NC--React-Movie-WebApp/index.html"
+  }
+];
